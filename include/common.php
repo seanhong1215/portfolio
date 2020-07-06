507dd9ced268cb7ab0aca67432a3a617e99bffa3
seanhong <seanhong@shoesconn.com>
2020-07-06 15:59:44 +0800

    final 0706

--- include/common.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>SEANHONG</title>
-<script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js"
-  integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous">
-</script>
-<link rel="icon" href="images/favicon.ico">
-<link rel="stylesheet" href="css/jquery.fancybox.css">
-<link rel="stylesheet" href="all.css">
